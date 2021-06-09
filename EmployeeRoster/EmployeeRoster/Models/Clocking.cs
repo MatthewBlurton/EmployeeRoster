6b9fcb3db4ae247acb5c6afc39344065dd15381d
MatthewBlurton <mjblurton@hotmail.com>
2021-06-09 17:23:37 +0800

    Created Entity Framework Core Models

---
+++ EmployeeRoster/EmployeeRoster/Models/Clocking.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeRoster.Models
+{
+    public class Clocking
+    {
+        public int Id { get; set; }
+        public DateTime Start { get; set; }
+        public long Duration { get; set; }
+    }
+}
